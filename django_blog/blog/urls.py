0680229890626cd91e9afd30e37efa5a10494e8b
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-10-06 19:19:56 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,10 +19,11 @@ urlpatterns = [
     path("posts/", PostListView.as_view(), name="post_list"),
     path("post/new/", PostCreateView.as_view(), name="post_create"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
-    path("post/<int:pk>/edit/", PostUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post_edit"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
 \
     
+
 
 
 
