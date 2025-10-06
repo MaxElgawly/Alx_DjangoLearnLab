6572e59654ca4843a6755faf0cdd556cbc1b7e8d
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-10-06 17:05:08 +0300

    Add files via upload

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,28 @@
+# blog/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+class EditProfileForm(UserChangeForm):
+    password = None  # Hide password field
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "first_name", "last_name"]
+
+# blog/forms.py
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "content"]
