1ababcffe6cd61a40a46f5d729c597ffbdf3a403
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-10-06 19:22:21 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -26,3 +26,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write a comment...'})
+        }
