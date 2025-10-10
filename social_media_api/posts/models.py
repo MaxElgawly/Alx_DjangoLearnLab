a408f877752877d5661001d7a7e3cfe2c72a4e84
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-10-10 12:44:24 +0300

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,29 +1,14 @@
 from django.db import models
-from django.conf import settings
+from accounts.models import CustomUser
 
 class Post(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f'{self.author.username}: {self.content[:30]}'
-   
-
-    def __str__(self):
-        return self.title
 
 
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'Comment by {self.author} on {self.post}'
-# Create your models here.
 
 
