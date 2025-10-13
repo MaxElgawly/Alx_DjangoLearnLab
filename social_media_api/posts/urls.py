537aeddd6aad91c464f46b79575a19fbd9702bf4
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-10-13 16:36:14 +0300

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import PostViewSet, CommentViewSet
 from django.urls import path
-from .views import FeedView
+from .views import FeedView, PostViewSet, CommentViewSet
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='post')
@@ -16,6 +16,8 @@ urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name='feed'),
 ]
+
+
 
 
 
