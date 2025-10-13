b8bc6a47b440b780819174151b659d59f5a863d0
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-10-13 15:20:50 +0300

    Update serializers.py

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,20 +1,30 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import Post, Comment
+
+User = get_user_model()
 
 class CommentSerializer(serializers.ModelSerializer):
     author_username = serializers.ReadOnlyField(source='author.username')
-
     class Meta:
         model = Comment
-        fields = ['id', 'author', 'author_username', 'post', 'content', 'created_at', 'updated_at']
-        read_only_fields = ['author', 'created_at', 'updated_at']
+        fields = ['id', 'post', 'author', 'author_username', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'author', 'author_username', 'created_at', 'updated_at']
 
+class PostListSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+    comments_count = serializers.IntegerField(source='comments.count', read_only=True)
 
-class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'author', 'author_username', 'content', 'comments_count', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'author', 'author_username', 'comments_count', 'created_at', 'updated_at']
+
+class PostDetailSerializer(serializers.ModelSerializer):
     author_username = serializers.ReadOnlyField(source='author.username')
     comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ['id', 'author', 'author_username', 'title', 'content', 'created_at', 'updated_at', 'comments']
-        read_only_fields = ['author', 'created_at', 'updated_at']
+        fields = ['id', 'title', 'author', 'author_username', 'content', 'comments', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'author', 'author_username', 'comments', 'created_at', 'updated_at']
