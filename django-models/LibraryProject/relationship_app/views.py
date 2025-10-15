ddeb94fcd743fa36fe7360e846d17b024be58fd8
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-10-15 23:03:08 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,12 +4,13 @@ from .models import Author, Book, Librarian, Library
 
 def list_all_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_book.html', {'books':books})
+    return render(request, 'templates/list_book.html', {'books':books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'templates/library_detail.html'
     context_object_name = 'library'
     
     def get_queryset(self):
         return super().get_queryset()()
+
