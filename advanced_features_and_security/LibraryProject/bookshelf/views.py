248541f39c1e559522734e64036c5e54fa447125
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-10-19 23:59:52 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required, login_required
+from .models import Book
+
+@login_required
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
+@login_required
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        description = request.POST.get('description')
+        Book.objects.create(title=title, author=author, description=description, created_by=request.user)
+        return redirect('book_list')
+    return render(request, 'bookshelf/create_book.html')
+
+
+@login_required
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.description = request.POST.get('description')
+        book.save()
+        return redirect('book_list')
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+
+@login_required
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book.delete()
+    return redirect('book_list')
 
 # Create your views here.
+
