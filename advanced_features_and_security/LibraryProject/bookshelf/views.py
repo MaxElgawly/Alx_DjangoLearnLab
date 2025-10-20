b4cb1e002f960ddcbf4ee47e44e72e8bf827e090
Maximose Elgawly <166640058+MaxElgawly@users.noreply.github.com>
2025-10-20 12:13:05 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required, login_required
 from .models import Book
+from .forms import BookForm
 
 @login_required
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -40,5 +41,24 @@ def delete_book(request, pk):
     book.delete()
     return redirect('book_list')
 
-# Create your views here.
+@login_required
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
+
+
+@login_required
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    search_query = request.GET.get('q', '')
+    # Safe ORM query (prevents SQL injection)
+    books = Book.objects.filter(title__icontains=search_query)
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
